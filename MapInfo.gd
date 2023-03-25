692f45a0e707c51bed7512b9614565d08450f3df
tmrupp <tmrupp@umich.edu>
2023-03-25 15:43:30 -0700

    toggle drawing map

--- MapInfo.gd
+++ MapInfo.gd
@@ -5,8 +5,6 @@ enum Type {
 	GROUND,
 }
 
-@onready var cam = $"../Camera2D"
-
 class Cell:
 	var type = Type.GROUND
 	
@@ -29,24 +27,24 @@ func _ready():
 			row.append(Cell.new(r))
 		cells.append(row)
 		
-	#top_left = get_viewport_rect().size
-	print("top_left", top_left)
 			
 	queue_redraw()
+
+var enabled = true
+func _input(event):
+	if event.is_action_pressed("Jump"):
+		enabled = !enabled
+		queue_redraw()
 			
 			
 func draw_cell(x, y, cell):
-	var color = Color.DARK_OLIVE_GREEN if cell.type == Type.GROUND else Color.BLACK
+	var color = (Color.DARK_OLIVE_GREEN if cell.type == Type.GROUND else Color.BLACK)
+	color.a = .5
 	draw_rect(Rect2(x*SPACING+top_left.x, y*SPACING+top_left.y, SPACING, SPACING), color)
 
 func _draw():
-#	print("drawing, len(cells)=", len(cells))
-	for i in len(cells):
-		for j in len(cells[i]):
-			# print("i=", i, " j=", j, " cell=", cells[i][j].type)
-			draw_cell(i, j, cells[i][j])
-			
-#	draw_rect(Rect2(1.0, 1.0, 3.0, 3.0), Color.GREEN)
-#	draw_rect(Rect2(5.5, 1.5, 2.0, 2.0), Color.GREEN, false, 1.0)
-#	draw_rect(Rect2(9.0, 1.0, 5.0, 5.0), Color.GREEN)
-#	draw_rect(Rect2(16.0, 2.0, 3.0, 3.0), Color.GREEN, false, 2.0)
+	if (enabled):
+		for i in len(cells):
+			for j in len(cells[i]):
+				# print("i=", i, " j=", j, " cell=", cells[i][j].type)
+				draw_cell(i, j, cells[i][j])
