669285bb9f77fe13c0fd00e3ad15bf3502945a05
nichgaun <nichgaun@umich.edu>
2023-03-26 18:00:06 -0400

    Update WaveFunctionCollapseGenerate.gd

--- WaveFunctionCollapseGenerate.gd
+++ WaveFunctionCollapseGenerate.gd
@@ -2,11 +2,17 @@ extends TileMap
 
 @export var width:int
 @export var height:int
+@export var patternSize:int
 
-var patterns # list of patterns
-var patternWeights # elements are pairwise with 'patterns'; represents weight of a given pattern in a sample (i.e. number of occurances)
+var patterns:Array[Pattern] # list of patterns
+var patternWeights:Array[int] # elements are pairwise with 'patterns'; represents weight of a given pattern in a sample (i.e. number of occurances)
+var totalWeight:int # the sum of the elements of 'patternWeights'; calculated once when sample array is read then never changed
 
-var possibleOptions # Options for each cell as the algorithm works out
+# Array[Array[Array[int]]] x/y grid and for each cell a list that is a set of indicies
+var currentOptions:Array # The current options possible for every grid square, actively changing while the algorithm runs, holds indicies into 'patterns'
+
+# Array[Array[int]] x/y grid and for each cell its entropy
+var currentEntropy:Array
 
 class Pattern:
 	# all patterns are square, this is the side length of the square
@@ -66,15 +72,23 @@ class Pattern:
 				if data[x][y] != other[x][y]:
 					return false
 		return true
+		
+	# returns Array[Vector2i]
+	static func generateDirectionListFromSize(size:int):
+		var directions = []
+		for x in range(-(size-1), size):
+			for y in range(-(size-1), size):
+				directions.append(Vector2i(x, y))
 
 # modifies 'patterns', 'patternWeights'
-func parseSampleForPatterns(sampleArray, patternSize:int):
+# sampleArray: Array[Array[int]] x/y grid and for each cell is an int identifying what type that cell is
+func parseSampleForPatterns(sampleArray:Array, patternSize:int):
 	patterns = []
 	patternWeights = []
 	# for each row
-	for x in range(sampleArray.size - patternSize + 1):
+	for x in range(sampleArray.size() - patternSize + 1):
 		# for each column
-		for y in range(sampleArray[0].size - patternSize + 1):
+		for y in range(sampleArray[0].size() - patternSize + 1):
 			# pull 'patternSize' number of rows starting from (x,y)
 			var pattern = []
 			for s in range(patternSize):
@@ -90,11 +104,88 @@ func parseSampleForPatterns(sampleArray, patternSize:int):
 					break
 			if not matchFound:
 				patterns.append(Pattern.new(patternSize, pattern))
+				patternWeights.append(1)
+				
+	totalWeight = 0
+	for i in patternWeights:
+		totalWeight += i
 
+# returns float
+func getEntropy(x:int, y:int):
+	var entropy:float = 0
+	
+	for optionIndex in currentOptions[x][y]:
+		var prob = float(patternWeights[optionIndex]) / totalWeight
+		entropy += prob * log(prob) / log(2)
+	
+	return -entropy
+
+func isEveryCellDecided():
+	for x in range(width):
+		for y in range(height):
+			if currentOptions.size() > 1:
+				return false
+	return true
+
+# returns Vector2i of coords of entry in grid with least entropy
+func getLeastNonzeroEntropy():
+	var least:float = INF
+	for x in range(width):
+		for y in range(height):
+			if currentEntropy[x][y] < least && currentEntropy[x][y] > 0:
+				least = currentEntropy[x][y]
+	return least
+
+# returns int
+func getWeightedRandomCellOption(pos:Vector2i):
+	var totalCurrentWeight:int = 0
+	for pattern in currentOptions[pos.x][pos.y]:
+		totalCurrentWeight += patternWeights[pattern]
+		
+	var rand:int = randi_range(0, totalCurrentWeight)
+	
+	var current:int = 0
+	var currentWeight:int = 0
+	while current < currentOptions.size():
+		if rand <= currentWeight:
+			return currentOptions[pos.x][pos.y][current]
+		currentWeight += patternWeights[currentOptions[pos.x][pos.y][current]]
+		current += 1
+	
+	return currentOptions[pos.x][pos.y][-1]
+
+func collapse(pos:Vector2i):
+	while currentOptions[pos.x][pos.y].size() > 0:
+		var collapsedValue:int = getWeightedRandomCellOption(pos)
+		currentOptions[pos.x][pos.y] = [collapsedValue]
+		currentEntropy[pos.x][pos.y] = 0
+		
+		# propagate to neighbors
+
+# modifies 'currentOptions'
+# modifies 'currentEntropy'
 func generateTerrainGrid(width:int, height:int):
-	pass
+	# fill 'possibleOptions' grid with all possible options, i.e. [0, 1, 2, ..., patterns.size()] for each cell
+	currentOptions = []
+	for x in range(width):
+		currentOptions.append([])
+		for y in range(height):
+			currentOptions[-1].append([])
+			for i in range(patterns.size()):
+				currentOptions[-1][-1].append(i)
+				
+	# fill 'currentEntropy' array
+	for x in range(width):
+		for y in range(height):
+			currentEntropy[x][y] = getEntropy(x, y)
+	
+	while not isEveryCellDecided():
+		var pos:Vector2i = getLeastNonzeroEntropy()
+		collapse(pos)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var sampleImage # TODO: read image information sample
+	parseSampleForPatterns(sampleImage, patternSize)
 	generateTerrainGrid(width, height)
-	pass # Replace with function body.
+	# TODO: set MapInfo values based on finished map
