96ed1cabd16980c5c83912d939c6554576d62cc6
tmrupp <tmrupp@umich.edu>
2023-03-26 12:07:02 -0700

    image converter

---
+++ ImageConverter.gd
@@ -0,0 +1,26 @@
+extends Node
+
+var icon_path = "res://icon.svg"
+
+func to_array(path):
+	var image = Image.load_from_file(path)
+	var values = []
+	var uniques = {}
+	
+	for i in image.get_size().x:
+		var row = []
+		for j in image.get_size().y:
+			var pixel = image.get_pixel(i, j)
+			if pixel not in uniques:
+				uniques[pixel] = len(uniques.keys())
+			print("pixel @ (", i, ", ", j, ") = ", pixel, " value=", uniques[pixel])
+			row.append(uniques[pixel])
+		values.append(row)
+		
+	return values
+	
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	to_array(icon_path)
+	pass # Replace with function body.
