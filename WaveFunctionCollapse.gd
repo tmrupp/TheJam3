343fe5a691ccbac72858cf98c6d6e5a796a8ff34
tmrupp <tmrupp@umich.edu>
2023-03-30 21:12:18 -0700

    c++

--- WaveFunctionCollapse.gd
+++ WaveFunctionCollapse.gd
@@ -1,17 +1,25 @@
 extends WaveFunctionCollapse
 
 
-func _input(event):
-#	print("got an event")
-	if event.is_action_pressed("Dash"):
-		set_size(Vector2i(10, 10))
-	if event.is_action_pressed("Jump"):
-		print("collapsing")
-		var map = collapse()
-		print(len(map))
+func to_uniques(data):
+	var uniques = {}
+	
+	var values = []
+	for j in range(len(data)):
+		var row = []
+		for i in range(len(data[j])):
+			var pixel = data[i][j]
+			if pixel not in uniques:
+				uniques[pixel] = len(uniques.keys())
+			print("pixel @ (", i, ", ", j, ") = ", pixel, " value=", uniques[pixel])
+			row.append(uniques[pixel])
+		values.append(row)
 		
+	return values
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("alive!")
-	pass # Replace with function body.
+	var mapInfo = get_tree().get_root().get_child(0).find_child("CanvasLayer").find_child("MapInfo")
+	var map = to_uniques(collapse())
+	mapInfo.load_all(map, map)
+	pass
