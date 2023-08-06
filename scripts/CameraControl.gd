e83ec0eca5ebb8a5e9a68e20b647a208cf07cf8b
nichgaun <nichgaun@umich.edu>
2023-08-06 02:54:38 -0400

    Smooth camera lerping

---
+++ scripts/CameraControl.gd
@@ -0,0 +1,36 @@
+extends Node
+
+# lerp method
+# always lerp the camera to the player
+const LERP_SMOOTHNESS = 0.04
+
+# bounding radius lerp method
+# only lerp to the player if the camera gets a certain distance away
+# once it does, lerp until we're within epsilon of the player
+const BOUNDING_RADIUS = 500
+const BR_LERP_SMOOTHNESS = 0.0001
+const BR_LOWER_THRESHOLD = 20
+var currently_lerping = false
+
+@onready var camera = $"../../Camera2D"
+@onready var player = $"../"
+
+# Set the position of the camera to lerp the player's position over time
+func _process(delta):
+	smooth_lerp(delta, LERP_SMOOTHNESS)
+	# bounding_radius_lerping(delta)
+
+# Move the camera towards the position of the player
+func smooth_lerp(delta, smoothness):
+	camera.position = lerp(camera.position, player.position, 1 - pow(smoothness, delta))
+
+# Check if the player/camera distance is above a threshold, lerp if so until we rest on the character again
+func bounding_radius_lerping(delta):
+	var diff = camera.position - player.position
+	print(diff.length())
+	if currently_lerping || diff.length() > BOUNDING_RADIUS:
+		smooth_lerp(delta, BR_LERP_SMOOTHNESS)
+		currently_lerping = true
+		
+		if diff.length() < BR_LOWER_THRESHOLD:
+			currently_lerping = false
