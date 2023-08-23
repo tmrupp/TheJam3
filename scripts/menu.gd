9230aa5367b51862652456f26fc8b2c4b5303d1f
tmrupp <tmrupp@umich.edu>
2023-08-23 09:06:34 -0700

    Randomize seed, copy, paste

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -4,6 +4,10 @@ extends CanvasLayer
 @onready var exit : Button = $VBoxContainer/Exit
 @onready var wfc : WaveFunctionCollapse = $"../WaveFunctionCollapse"
 
+@onready var randomize : Button = $"VBoxContainer/World Seed/Randomize"
+@onready var copy : Button = $"VBoxContainer/World Seed/Copy"
+@onready var paste : Button = $"VBoxContainer/Map Seed/Paste"
+
 @onready var world_seed = $"VBoxContainer/World Seed/LineEdit"
 @onready var map_seed = $"VBoxContainer/Map Seed/LineEdit"
 
@@ -28,10 +32,22 @@ func _input(event):
 		if main.has_node("Player"): #!= null:
 			visible = !visible
 
+func randomize_seed ():
+	world_seed.text = str(randi())
+
+func copy_seed ():
+	DisplayServer.clipboard_set(world_seed.text)
+
+func paste_seed ():
+	map_seed.text = DisplayServer.clipboard_get()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	start.pressed.connect(start_game)
 	exit.pressed.connect(exit_game)
+	randomize.pressed.connect(randomize_seed)
+	copy.pressed.connect(copy_seed)
+	paste.pressed.connect(paste_seed)
 	pass # Replace with function body.
 
 
