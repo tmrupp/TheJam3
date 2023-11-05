baed845cca7094fc8c9ae3b1e452a82a26cb0df7
nichgaun <nichgaun@umich.edu>
2023-11-05 14:32:32 -0500

    Added sfx for coin pickup

--- scripts/Coins.gd
+++ scripts/Coins.gd
@@ -2,10 +2,13 @@ extends Node
 
 var coins: int = 100
 @onready var amount: Label = $"/root/Main/CanvasLayer/HUD/TopHUD/CoinAmount"
+@onready var coin_collect_sfx = $AudioStreamPlayer
 
 func modify (delta: int) -> void:
 	coins += delta
 	display()
+	if delta > 0:
+		coin_collect_sfx.play()
 
 func display () -> void:
 	amount.text = ": " + str(coins)
