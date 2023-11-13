e1932ec1a3414c6bc6b68c9eaa9ed74231eee145
nichgaun <nichgaun@umich.edu>
2023-11-12 20:20:13 -0500

    Added camera shake to parry and taking damage

--- prefabs/scenes/CameraEffects.gd
+++ prefabs/scenes/CameraEffects.gd
@@ -2,14 +2,25 @@ extends Camera2D
 
 var stored_delta: float = 0
 var rng = RandomNumberGenerator.new()
-	
+
+# in order to have access to delta time in a non-process function
+# we save it into a stored value here
 func _process(delta):
 	stored_delta = delta
 
-func shake(intensity: float, direction: Vector2 = Vector2.ZERO) -> void:
-	while intensity > 0.01:
-		print("intensity: " + str(intensity))
-		offset = Vector2(rng.randf_range(-intensity, intensity), rng.randf_range(-intensity, intensity))
-		intensity = lerpf(intensity, 0, 1 - pow(0.1, stored_delta))
+# shake the camera by applying random offsets to the camera's location
+# intensity: the radius of square within which a single camera position jump can be
+# sustain: the time, in seconds, that the camera will be shaking
+#
+# as the duration of the shake goes on, the intensity is linearly decayed to 0
+func shake(intensity: float, sustain: float, direction: Vector2 = Vector2.ZERO) -> void:
+	var current_intensity = intensity
+	var accumulated_time = 0
+	
+	while accumulated_time <= sustain:
+		offset = Vector2(rng.randf_range(-current_intensity, current_intensity), rng.randf_range(-current_intensity, current_intensity))
+		current_intensity = lerpf(intensity, 0, accumulated_time / sustain)
+		accumulated_time += stored_delta
 		await get_tree().process_frame
+		
 	offset = Vector2.ZERO
