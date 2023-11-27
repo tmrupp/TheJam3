3450e405bdf9d258f2734a1e215b4845262e1719
tmrupp <tmrupp@umich.edu>
2023-11-27 13:50:51 -0700

    goal interactible

--- scripts/goal.gd
+++ scripts/goal.gd
@@ -14,12 +14,10 @@ func setup(_map_info: MapInfo, _v: Vector2i) -> void:
 func crack (map_code: String) -> void:
 	upgrade_menu.present()
 	map_info.generate(code, map_code)
-
-func touch(other: Node) -> void:
-	if (other == player and other.get_parent() != null):
-#		print("self=", self, " player.position=", player.position, " collision=", player.get_collision())
-		code_menu.enable(code, map_info.all_map_codes.keys(), crack)
+	
+func interacted () -> void:
+	code_menu.enable(code, map_info.all_map_codes.keys(), crack)
 		
 func _ready() -> void:
-	connect("body_entered", touch)
+	$Interactable.connect("interacted", interacted)
 	
